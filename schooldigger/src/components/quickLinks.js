37b10577c0e827f6e9b7a6dc4f6a54699933b8f5
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-22 12:33:03 -0500

    added links for external resources

---
+++ schooldigger/src/components/quickLinks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function QuickLinks() {
+    return (
+    <div className="header">
+            <h3 className="title">QuickLinks</h3>
+      </div>
+
+    );
+};
+    
+export default QuickLinks;
