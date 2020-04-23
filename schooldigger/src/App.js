9b1b0820b04438d2c1be2c954ea581b0be96e545
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-22 20:34:43 -0500

    sending props to child components

--- schooldigger/src/App.js
+++ schooldigger/src/App.js
@@ -2,30 +2,70 @@ import React,{Component} from 'react';
 import './App.scss';
 import Header from './components/header'
 import Footer from './components/footer'
+import Home from './components/home'
 import BrowserHistory from './components/browserHistory'
 import QuickLinks from './components/quickLinks'
 import SchoolsList from './components/schoolsList'
-import Navbar from './components/navbar'
 import {Route,Switch,Link,BrowserRouter as Router} from 'react-router-dom'
+import axios from 'axios'
+import { Button } from 'react-bootstrap';
 
 
+const apiKey=process.env.REACT_APP_API_KEY
+const appId=process.env.REACT_APP_ID
 
 class  App extends Component {
   constructor(props){
     super(props);
-    this.OnSearch = this.OnSearch.bind(this);
-  }
-  OnSearch(e){
-    alert("Search clicked");
-    e.preventDefault();
+    this.state={
+        city:'',
+        state:'',
+        schools:[],
+        isLoaded:false
+    }
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleCityChange = this.handleCityChange.bind(this);
+    this.handleStateChange = this.handleStateChange.bind(this);
 
+  }
+  handleCityChange(e){
+      this.setState({city:e.target.value})
+  }
+  handleStateChange(e){
+    this.setState({state:e.target.value})
+}
+  handleSearch(e){
+      alert("Search clicked");
+      const city = this.state.city;
+      const state = this.state.state;
+      const url = `https://api.schooldigger.com/v1.2/schools?st=${state}&city=${city}&sortBy=rank&appID=${appId}&appKey=${apiKey}`;
+      axios.headers={
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+    }
+      axios.get(url)
+      .then(res => {
+        const results = res.data;
+        const schoolList = results.schoolList;        
+        this.state.schools.push(...schoolList);
+        this.setState({isloaded:true})  
+        console.log("this.setState.school",this.state.schools)     
+      })
+      .catch(error => {
+        console.log('there is an eror', error)
+      })
+      e.preventDefault();
   }
   render(){
     return (
       <div className="App">
         <div className="header">
           <Header/>
-          <Navbar onClick={this.OnSearch}/>
+          <>
+            <input type='text' className='input' name='city' placeholder='Plano' value={this.setState.city} onChange={this.handleCityChange}></input>
+            <input type='text' className='input' name='state' placeholder='TX' value={this.setState.state} onChange={this.handleStateChange}></input>
+            <Button variant="primary" type="submit" onClick={this.handleSearch}> Search</Button>
+          </>
         </div>
         <div className="body">
                 <Router>  
@@ -33,24 +73,29 @@ class  App extends Component {
                   <nav> 
                       <ul className="menu">
                       <li>
-                        <Link to="/" ></Link>
+                        <Link  to={{ pathname: '/', state: {schoolList: this.state.schools } }} ></Link>
                       </li>
                       <li>
-                        <Link to="/" >My Schools List</Link>
+                        <Link  to={{ pathname: '/SchoolsList', state: {schoolList: this.state.schools } }} >My Schools List</Link>
+                      </li>
+                      <li>
+                        <Link to="/SearchHistory">Search History</Link>
                       </li>
                       <li>
                         <Link to="/QuickLinks" >Resources</Link>
                       </li>
-                     
-                      <li>
-                        <Link to="/SearchHistory">Search History</Link>
-                      </li>
-                    </ul>
+                    
+                     </ul>
                   </nav>
                   <Switch>
-                       <Route exact path="/SearchHistory" component={BrowserHistory}>          
+                      <Route exact path="/"  component={(props) => <Home {...props} data={this.state.schoolList}/>} />     
+                      <Route exact path="/SearchHistory" component={BrowserHistory}>  
+                       {this.props.children}
+        
                       </Route> 
-                      <Route exact path="/SchoolsList" component={SchoolsList}>          
+                      <Route exact path="/SchoolsList" component={SchoolsList}>  
+                      {this.props.children}
+        
                       </Route> 
                       <Route exact path="/QuickLinks" component={QuickLinks}>          
                       </Route>             
