9e4efaecc22807e9991052fa31eca6f6e6c445bf
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-24 11:44:53 -0500

    cities link added

--- schooldigger/src/components/quickLinks.js
+++ schooldigger/src/components/quickLinks.js
@@ -4,7 +4,8 @@ function QuickLinks() {
     <div className="main">
         <div className="sub-main-2">
             <div className="container">
-                <h3 className="title">QuickLinks</h3>
+                <h3 className="title">List of cities and towns in the United States</h3>
+                <a href="https://www.britannica.com/topic/list-of-cities-and-towns-in-the-United-States-2023068">Cities List</a>
             </div>
 
         </div>
