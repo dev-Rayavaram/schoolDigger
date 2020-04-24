c42a0d4ae79e1c271a8e29b9b76da6b6e40b9b99
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-24 17:59:21 -0500

    added pseudocode for Header component

--- schooldigger/src/components/header.js
+++ schooldigger/src/components/header.js
@@ -1,8 +1,13 @@
 import React from 'react';
+/*
+  DECLARE stateless component Header
+  add title
+  export Header
+*/
 function Header() {
     return (
     <div className="header">
-            <h3 className="title">Start Your Search by City</h3>
+            <h3 className="title">Start Your Search by City and State</h3>
       </div>
 
     );
