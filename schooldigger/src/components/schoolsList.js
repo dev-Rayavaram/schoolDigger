e735de561600a1e0bbacb6615e6d91110ba2f374
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-24 18:42:22 -0500

    pseudocode added for creating and initializing a class component

--- schooldigger/src/components/schoolsList.js
+++ schooldigger/src/components/schoolsList.js
@@ -1,4 +1,9 @@
 import React,{Component} from 'react';
+/*
+    create a class component
+    initialize with empty state variables
+    export component
+*/
 class SchoolsList extends Component {
     constructor(props){
         super(props)
@@ -8,25 +13,25 @@ class SchoolsList extends Component {
         }
     }
     componentDidMount=()=>{
-        console.log("schools list data componentDidMount",this.props.location.state)
+      //  console.log("schools list data componentDidMount",this.props.location.state)
 
         if(this.props.location.state !==null && this.props.location.state!==undefined){
             let items = this.props.location.state;
-            console.log("inside componentDidMount items" ,items)
+       //     console.log("inside componentDidMount items" ,items)
             if(items.length !==0){
                 let localSchoolsList = items.schoolList;
                 localSchoolsList.map((school,index)=>this.state.schoolsList.push(school)) 
                 this.setState({isLoaded:true})
-                console.log("inside componentDidMount items ",this.state.schoolsList);         
+             //   console.log("inside componentDidMount items ",this.state.schoolsList);         
             }
          }
-        console.log("schools list data in SchoolsList Class",this.state.schoolsList)
+    //    console.log("schools list data in SchoolsList Class",this.state.schoolsList)
     }
     render=()=>{
             if(this.state.isLoaded===true && this.state.schoolsList.length !==0){
-                console.log("inside schoollist render",this.state.schoolsList[0])
+           //     console.log("inside schoollist render",this.state.schoolsList[0])
                 let items = this.state.schoolsList;
-                console.log("items inside schoolList render",items)
+           //     console.log("items inside schoolList render",items)
                 return (
                     <div className="main">
                         <div className="sub-main-2">
