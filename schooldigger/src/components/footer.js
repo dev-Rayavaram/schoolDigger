9e1a8775f1381be2ca644e9cc0d77f16ef568c48
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-24 00:47:31 -0500

    added quotation to the footer

--- schooldigger/src/components/footer.js
+++ schooldigger/src/components/footer.js
@@ -3,9 +3,10 @@ import React from 'react';
 function Footer() {
     return (
     <div className="footer">
-            <h2><i>Channels</i></h2>
-            <h5>TURN IN ON TO <i>NAUTILUS' </i> DEEP DIVE PORTALS</h5>
-            
+            <p>
+            “Education is the passport to the future, for tomorrow belongs to those who prepare for it today.”
+            Malcolm X
+            </p>           
     </div>
 
     );
