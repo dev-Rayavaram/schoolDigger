25456bc0b7e096130eedd2cf272fdfea1b4e2171
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-23 19:49:45 -0500

    added functionality to trigger App.js event from child component in home

--- schooldigger/src/components/home.js
+++ schooldigger/src/components/home.js
@@ -1,5 +1,6 @@
-import React,{Component,useState} from 'react';
+import React,{Component,useState, useReducer} from 'react';
 import { Button } from 'react-bootstrap';
+import BrowserHistory from './browserHistory';
 
 
 class Home extends Component {
@@ -7,10 +8,11 @@ class Home extends Component {
         super(props)
         this.state={
             schoolsList:[],
+            savedSchoolsList:[],
             isLoaded:false
         }
     }
-   
+ 
     componentDidMount=()=>{
         console.log("home componentDidMount",this.props.data)
 
@@ -28,7 +30,7 @@ class Home extends Component {
                     <div className="main">
                         <div className="sub-main-1">
                             {Object.values(this.state.schoolsList).map((item,index)=>{return(
-                                <Item key={index} value={item} data={item}/>
+                                <Item key={index} value={item} data={item} showSavedList={this.props.showSavedList}/>
                             )})}  
                         </div>
                     </div>
@@ -48,21 +50,25 @@ class Home extends Component {
          
     }
  };
-    
+
+ 
 export default Home;
-/*The following function uses react HOOKs for maintaining state in stateless components that are 
- functional components*/
-//https://www.youtube.com/watch?v=-MlNBTSg_Ww
+
+/*  The following function uses react HOOKs for maintaining state in stateless components that are 
+    functional components using useState,useReducer. useState is similar to setState in class component
+    and useReducer behaves similar to Redux reducer and here are the links for resources
+    showSavedList is reducer action and addItem is uset to set new state
+    https://www.youtube.com/watch?v=-MlNBTSg_Ww
+    https://medium.com/javascript-in-plain-english/how-to-add-to-an-array-in-react-state-3d08ddb2e1dc
+*/
 const Item=(props)=>{
-    const[state,addItem]=useState({
-       schoolName:''
-    })
-    
+    const[state,addItem]=useState([]);
     const handleAdd=(e)=>{
         e.preventDefault();
         const schoolName=e.target.value;
-        addItem(schoolName);
+        addItem(state => [...state, schoolName])
         console.log("state inside function",state)
+        props.showSavedList(state)
     }
     console.log("props inside functional ",props)
     let ranks = JSON.stringify(props.value.rankHistory)
@@ -73,7 +79,7 @@ const Item=(props)=>{
                 <h3>Phone:{props.value.phone}</h3>
                 <a href={props.value.url}>Link</a>
                 <h5 id="rank">Rank:{ranks}</h5>
-                <button type="button" value ={props.value.schoolName}  onClick={handleAdd.bind(null)}>Favorites</button>
+                <Button type="button" value ={props.value.schoolName}  onClick={handleAdd.bind(null)}>Favorites</Button>
             </div>
          </React.Fragment>
 
