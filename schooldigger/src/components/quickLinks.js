0bfaeb66c58fd47b38d84e6d56b51e4f7b952cba
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-24 00:50:15 -0500

    changed classname

--- schooldigger/src/components/quickLinks.js
+++ schooldigger/src/components/quickLinks.js
@@ -2,7 +2,12 @@ import React from 'react';
 function QuickLinks() {
     return (
     <div className="main">
-            <h3 className="title">QuickLinks</h3>
+        <div className="sub-main-2">
+            <div className="container">
+                <h3 className="title">QuickLinks</h3>
+            </div>
+
+        </div>
       </div>
 
     );
