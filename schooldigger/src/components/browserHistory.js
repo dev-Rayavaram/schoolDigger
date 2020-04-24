cad405f6748e107d9ed2ab9eba01bcb85ec4a85b
dev.rayavaram <dev.rayavaram@gmail.com>
2020-04-23 22:04:55 -0500

    previous search location rendered

---
+++ schooldigger/src/components/browserHistory.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function BrowserHistory(props) {
+    if(props.location.state !==null && props.location.state!==undefined){
+        return (
+            <div className="main">
+                <div className="sub-main-2">
+                        <ul>
+                            <>
+                            {Object.values(props.location.state).map((item,index)=>
+                                <li key={index}>
+                                   <h3>Your previous search location:</h3> {item[0]}{item[1]}
+                                </li>
+                            )}  
+
+                             </>
+                        </ul>
+                </div>
+             </div>
+ 
+        );
+
+    }
+    else{
+        return (
+            <div className="main">
+                Search to get results                    
+            </div>
+        )    
+    }
+};
+    
+export default BrowserHistory;
